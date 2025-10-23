4040bd7a78c86e4f7821c62e826cfc37375b4117
H11iye <hamdi.iyed11@gmail.com>
2025-10-23 15:37:43 +0000

    Update the main file in aws_s3_lambda module

--- infra/modules/aws_s3_lambda/main.tf
+++ infra/modules/aws_s3_lambda/main.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "input_bucket" {
   tags = {
     Environment = var.environment
     Purpose = "Lambda Input Bucket"
+    Project = var.project
   }
 }
 
@@ -23,31 +24,36 @@ resource "aws_s3_bucket" "output_bucket" {
 # IAM role for Lambda
 
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.environment}-lambda-role"
+  name = "${var.environment}-${var.project}-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
         {
+            Action = "sts:AssumeRole"
             Effect = "Allow"
             Principal = {
                 Service = "lambda.amazonaws.com"
             }
-            Action = "sts:AssumeRole"
         }
     ]
   })
+  tags = {
+    Environment = var.environment
+    Project = var.project
+  }
 }
 
-# IAM Policy for Lambda Permissions
+# Inline IAM Policy for Lambda Permissions
 
-resource "aws_iam_policy" "lambda_policy" {
-  name = "${var.environment}-lambda-policy"
-  description = "Allow Lambda to read input bucket and write to output bucket"
+resource "aws_iam_policy" "lambda_inline_policy" {
+  name = "${var.environment}-${var.project}-lambda-policy"
+  description = "Policy for Lambda S3 to read/write and CloudWatch logs"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
         {
+            Sid = "S3InputRead"
             Effect = "Allow"
             Action = [
                 "s3:GetObject",
@@ -59,6 +65,17 @@ resource "aws_iam_policy" "lambda_policy" {
             ]
         },
         {
+            Sid = "S3OutputWrite"
+            Effect = "Allow"
+            Action = [
+                "logs:PutObject"
+            ]
+            Resource = [
+                "${aws_s3_bucket.output_bucket.arn}/*"
+            ]
+        },
+        {
+            Sid = "CloudWatchLogs"
             Effect = "Allow"
             Action = [
                 "logs:CreateLogGroup",
@@ -73,30 +90,56 @@ resource "aws_iam_policy" "lambda_policy" {
 
 # Attach Policy to Role
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+resource "aws_iam_role_policy_attachment" "attach_policy" {
   role = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy" "inline_attach" {
+  name = "${var.environment}-${var.project}-inline"
+  role = aws_iam_role.lambda_role.id
+  policy = aws_iam_policy.lambda_inline_policy.policy
+}
+
+# Lambda either from ZIP (local->S3 or local copy) or from container image in ECR
 # Lambda Function
 
 resource "aws_lambda_function" "lambda_function" {
-  function_name = "${var.environment}-s3-processor"
+    filename = var.lambda_zip_path != "" ? var.lambda_zip_path : null
+  function_name = "${var.environment}-${var.project}-processor"
   role = aws_iam_role.lambda_role.arn
   handler = var.lambda_handler
   runtime = var.lambda_runtime
-  filename = var.lambda_zip_path
-  timeout = 30
+  source_code_hash = var.lambda_source_code_hash != "" ? var.lambda_source_code_hash : null
+  publish = true
+  timeout = var.lambda_timeout
 
   environment {
-    variables = {
+    variables = merge({
       OUTPUT_BUCKET = aws_s3_bucket.output_bucket.bucket
-    }
+      INPUTBUCKET = aws_s3_bucket.input_bucket.bucket
+      ENVIRONMENT = var.environment
+      PROJECT = var.project
+    }, var.extra_environment_vars)
   }
 
   tags = {
     Environment = var.environment
+    Project = var.project
   }
+
+  lifecycle {
+    ignore_changes = var.lambda_ignore_changes ? [environment] : []
+  }
+}
+
+# S3 -> Lambda notification
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id = "AllowExecutionFromS3"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.function_name
+  principal = "s3.amazonaws.com"
+  source_arn = aws_s3_bucket.input_bucket.arn
 }
 
 # S3 Event Notification to Trigger Lambda
@@ -108,9 +151,10 @@ resource "aws_s3_bucket_notification" "input_notification" {
     lambda_function_arn = aws_lambda_function.lambda_function.arn
     events = ["s3:ObjectCreated:*"]
     filter_prefix = var.prefix_filter
+    filter_suffix = var.suffix_filter
   }
 
-  depends_on = [ aws_lambda_function.lambda_function ]
+  depends_on = [ aws_lambda_permission.allow_s3 ]
 }
 
 # Lambda Permission for S3
@@ -121,4 +165,34 @@ resource "aws_lambda_permission" "allow_s3" {
   function_name = aws_lambda_function.lambda_function.function_name
   principal = "s3.amazonaws.com"
   source_arn = aws_s3_bucket.input_bucket.arn
+}
+
+# Versioning via seperate resource
+
+resource "aws_s3_bucket_versioning" "input_bucket_versioning" {
+  bucket = aws_s3_bucket.input_bucket.id
+
+  versioning_configuration {
+    status = var.enable_versioning ? "Enabled" : "Suspended"
+  }
+}
+
+# Public access & ownership controls 
+
+resource "aws_s3_bucket_public_access_block" "input_bucket_block" {
+  bucket = aws_s3_bucket.input_bucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "input_bucket_ownership" {
+  bucket = aws_s3_bucket.input_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
 }
--- infra/modules/aws_s3_lambda/variables.tf
+++ infra/modules/aws_s3_lambda/variables.tf
@@ -8,6 +8,12 @@ variable "environment" {
   type = string
 }
 
+variable "project" {
+  description = "dev|staging|prod"
+  type = string
+  default = "GITOPS_Pro"
+}
+
 variable "input_bucket_name" {
   description = "Name of the S3 input bucket"
   type = string
@@ -37,4 +43,34 @@ variable "prefix_filter" {
   description = "Prefix filter for S3 event trigger"
   type = string
   default = ""
+}
+
+variable "enable_versioning" {
+  description = "Whether versioning should be enabled on the bucket"
+  type = bool
+  default = true
+}
+
+variable "block_public_acls" {
+  description = "Whether to block public ACLs on the bucket"
+  type = bool
+  default = true
+}
+
+variable "block_public_policy" {
+    description = "Whether to block public bucket policies"
+    type = bool
+    default = true
+}
+
+variable "ignore_public_acls" {
+  description = "To restrict public buckets"
+  type = bool
+  default = true
+}
+
+variable "object_ownership" {
+  description = "Object ownership setting for the bucket"
+  type = string
+  default = "BucketOwnerEnforced"
 }
