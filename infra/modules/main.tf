cb760c9c51cabb334556f60668cca21aef031a37
H11iye <hamdi.iyed11@gmail.com>
2025-10-29 15:25:48 +0000

    Add EKS node group

--- infra/modules/main.tf
+++ infra/modules/main.tf
@@ -228,3 +228,65 @@ resource "aws_eks_cluster" "main" {
      Name = var.cluster_name
    }
 }
+
+# EKS Node Group IAM Role
+resource "aws_iam_role" "eks-nodes" {
+  name = "${var.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks-worker-node-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role = aws_iam_role.eks-nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cni-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.eks-nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-container-registry-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role = aws_iam_role.eks-nodes.name
+}
+
+# EKS Node Group 
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = aws_subnet.private[*].id
+  instance_types  = var.node_instance_types
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_policy,
+  ]
+
+  tags = {
+    Name = "${var.cluster_name}-node-group"
+  }
+}
+
+# Configure kubectl
