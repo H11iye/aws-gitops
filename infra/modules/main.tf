3b3ca34118e3b652d2c8d9364e4998b221d5779a
H11iye <hamdi.iyed11@gmail.com>
2025-10-29 15:35:47 +0000

    Add the kubernetes conf to IaC

--- infra/modules/main.tf
+++ infra/modules/main.tf
@@ -290,3 +290,22 @@ resource "aws_eks_node_group" "main" {
 }
 
 # Configure kubectl
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.main.name
+}
+
+provider "kubernetes" {
+  host = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.cluster_authority[0].data)
+  token = data.aws_eks_cluster_auth.main.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token = data.aws_eks_cluster_auth.main.token
+  }
+}
+
+# AWS Load Balancer Controller IAM Policy
